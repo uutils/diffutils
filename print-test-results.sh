a526208ed856dc01215a216836fb8e1efdb62d74
Olivier Tilloy <olivier@tilloy.net>
2024-02-15 21:02:22 +0100

    Add a script to print the test results

---
+++ print-test-results.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Print the test results written to a JSON file
+# by run-upstream-testsuite.sh in a markdown format.
+
+json="test-results.json"
+[[ -n $1 ]] && json="$1"
+
+codeblock () { echo "\`\`\`"; }
+
+jq -c '.tests[]' "$json" | while read -r test
+do
+  name=$(echo "$test" | jq -r '.test')
+  echo "# test: $name"
+  result=$(echo "$test" | jq -r '.result')
+  echo "result: $result"
+  url=$(echo "$test" | jq -r '.url')
+  echo "url: $url"
+  if [[ "$result" != "SKIP" ]]
+  then
+    stdout=$(echo "$test" | jq -r '.stdout' | base64 -d)
+    if [[ -n "$stdout" ]]
+    then
+      echo "## stdout"
+      codeblock
+      echo "$stdout"
+      codeblock
+    fi
+    stderr=$(echo "$test" | jq -r '.stderr' | base64 -d)
+    if [[ -n "$stderr" ]]
+    then
+      echo "## stderr"
+      codeblock
+      echo "$stderr"
+      codeblock
+    fi
+  fi
+  echo ""
+done
